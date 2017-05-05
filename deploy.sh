e665d71337ee0ff34cf8878229e83d06101e1442
Pawel Psztyc <jarrodek@gmail.com>
2017-05-05 21:17:42 +0100

    Hopefully, fixed key decryption to access github

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
+eval `ssh-agent -s`
+ssh-add gh-travis_rsa
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy."
@@ -51,16 +54,6 @@ echo "Deploying..."
 # The delta will show diffs between new and old versions.
 git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../gh-travis_rsa.enc -out ../gh-travis_rsa -d
-chmod 600 ../gh-travis_rsa
-eval `ssh-agent -s`
-ssh-add gh-travis_rsa
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
